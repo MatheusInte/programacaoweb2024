03e27f3eb702afe547d7f4856561a035a251811e
Mateus Almeida <mateusva.1132@gmail.com>
2024-10-10 14:02:41 -0300

    fix: anotações da classe Usuario

--- src/main/java/com/programacaoweb2024/entities/Usuario.java
+++ src/main/java/com/programacaoweb2024/entities/Usuario.java
@@ -2,10 +2,14 @@ package com.programacaoweb2024.entities;
 
 import com.programacaoweb2024.enums.UsuarioEnum;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 
+@RequiredArgsConstructor
+@AllArgsConstructor
 @Data
 @Entity
 public class Usuario {
