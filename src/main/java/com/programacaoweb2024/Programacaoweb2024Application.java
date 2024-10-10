5cd2fd1c83bd59d728760f3a1202a01052a53138
MatheusInte <matheus.interaminense.guerra@gmail.com>
2024-10-09 22:45:24 -0300

    feat: configuracao de ambiente

---
+++ src/main/java/com/programacaoweb2024/Programacaoweb2024Application.java
@@ -0,0 +1,13 @@
+package com.programacaoweb2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Programacaoweb2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Programacaoweb2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/programacaoweb2024/Programacaoweb2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacaoweb2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Programacaoweb2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
