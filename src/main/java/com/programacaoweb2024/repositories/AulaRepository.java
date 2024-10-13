91ccc72003b4b534c75859c6d8853d496b283801
luisgcpsantos <gustavosantossbu@gmail.com>
2024-10-13 19:57:42 -0300

    feat: criação dos repositorios

---
+++ src/main/java/com/programacaoweb2024/repositories/AulaRepository.java
@@ -0,0 +1,7 @@
+package com.programacaoweb2024.repositories;
+
+import com.programacaoweb2024.entities.Aula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AulaRepository extends JpaRepository<Aula, Long> {
+}
---
+++ src/main/java/com/programacaoweb2024/repositories/ExercicioRepository.java
@@ -0,0 +1,7 @@
+package com.programacaoweb2024.repositories;
+
+import com.programacaoweb2024.entities.Exercicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExercicioRepository extends JpaRepository<Exercicio, Long> {
+}
